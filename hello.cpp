bc551f49a195e351f472db0c0c4a4619fcb2b237
Your Name <preritshrivastava001@gmail.com>
2021-03-04 19:27:36 +0530

    First Commit

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char * str="Prerit Shrivastava";
+    cout<<"Hi I am "<<str<<endl;
+    cout<<"This is my first Git program.";
+    return 0;
+}
