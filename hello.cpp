b3325541c8f202b4f037b7f33285b5d9e4d39964
Your Name <preritshrivastava001@gmail.com>
2021-03-04 19:38:05 +0530

    Second Commit

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     char * str="Prerit Shrivastava";
-    cout<<"Hi I am "<<str<<endl;
+    cout<<"Hi I am "<<str<<endl;    //Enter your name
     cout<<"This is my first Git program.";
     return 0;
 }
